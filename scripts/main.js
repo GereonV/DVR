ccc2b8a0952c9aec6276b25cd66e47a5a74456f7
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-03-15 20:36:11 +0100

    Add connection support

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,83 @@
-const NAME_TEXT = document.getElementById("name");
-const IP_TEXT = document.getElementById("ip");
-const ROUTER_CHECKBOX = document.getElementById("router");
-const AREA_DIV = document.getElementById("area");
-AREA_DIV.addEventListener("click", areaDivClick);
+const AREA = document.getElementById("area");
+const SVG = document.getElementById("svg");
+
+AREA.addEventListener("click", clickRouterAdd);
+AREA.addEventListener("click", clickConnection);
 
 const network = new Network;
+const connections = new Map; // [ip1, ip2] -> line
 
-function areaDivClick(event) {
-	const areaDiv = event.currentTarget;
+function clickRouterAdd(event) {
 	const x = event.offsetX;
 	const y = event.offsetY;
-	if(event.target !== areaDiv || !isPositionValid(x, y))
+	if(event.button !== 0 || event.target !== AREA || !isPositionValid(x, y))
 		return;
+	const element = createRouter();
+	if(!element)
+		return;
+	element.addEventListener("mousedown", event => {
+		if(!event.ctrlKey)
+			return;
+		const centerToCursorX = element.offsetWidth  / 2 - event.offsetX;
+		const centerToCursorY = element.offsetHeight / 2 - event.offsetY;
+		AREA.addEventListener("mousemove", move);
+		AREA.addEventListener("mouseup", stop);
+		function move(event) {
+			if(event.target !== AREA)
+				return;
+			const x = event.offsetX + centerToCursorX;
+			const y = event.offsetY + centerToCursorY;
+			if(!isPositionValid(x, y))
+				return;
+			toPosition(element, x, y);
+			for(other of network.getNeighborsOf(element.id))
+				moveLineBetween(connections.get(getConnectionString(element.id, other)), element, document.getElementById(other));
+		}
+		function stop() {
+			AREA.removeEventListener("mousemove", move);
+			AREA.removeEventListener("mouseup", stop);
+		}
+	});
+	AREA.appendChild(element);
+	toPosition(element, x, y);
+}
+
+let selectedElement = null;
+function clickConnection(event) {
+	if(event.button !== 0 || !event.shiftKey || event.target === AREA)
+		return;
+	if(!selectedElement) {
+		selectedElement = event.target;
+		selectedElement.classList.add("selected");
+		return;
+	}
+	const element1 = selectedElement;
+	const element2 = event.target;
+	const ip1 = element1.id, ip2 = element2.id;
+	selectedElement.classList.remove("selected");
+	selectedElement = null;
+	if(element1 === element2)
+		return;
+	if(network.getConnection(ip1, ip2) !== undefined) {
+		const distance = promptDistance();
+		if(distance !== null)
+			network.setConnection(ip1, ip2, distance);
+		return;
+	}
+	const line = createLine(ip1, ip2);
+	if(line !== null)
+		moveLineBetween(line, element1, element2);
+}
+
+function createRouter() {
 	let ip;
 	do {
 		ip = prompt("Enter IP");
 		if(ip === null)
-			return;
-	} while(!isValidIP(ip));
-	if(!network.addRouter(ip))
-		return;
-	const element = createRouter(ip);
-	element.addEventListener("mousedown", clickEvent => {
-		const centerToCursorX = element.offsetWidth  / 2 - clickEvent.offsetX;
-		const centerToCursorY = element.offsetHeight / 2 - clickEvent.offsetY;
-		areaDiv.addEventListener("mousemove", move);
-		areaDiv.addEventListener("mouseup", stop);
-		function move(moveEvent) {
-			if(moveEvent.target !== moveEvent.currentTarget)
-				return;
-			const x = moveEvent.offsetX + centerToCursorX;
-			const y = moveEvent.offsetY + centerToCursorY;
-			if(isPositionValid(x, y))
-				toPosition(element, x, y);
-		}
-		function stop() {
-			areaDiv.removeEventListener("mousemove", move);
-			areaDiv.removeEventListener("mouseup", stop);
-		}
-	});
-	element.addEventListener("mousedown", evt => {
-		// TODO connection
-	});
-	event.currentTarget.appendChild(element);
-	toPosition(element, x, y);
-}
-
-function createRouter(ip) {
+			return null;
+	} while(!isValidIP(ip) || !network.addRouter(ip));
 	const element = document.createElement("div");
+	element.id = ip;
 	element.className = "router";
 	element.appendChild(document.createTextNode(ip));
 	return element;
@@ -69,109 +99,36 @@ function toPosition(element, x, y) {
 	element.style.left = (x - element.offsetWidth / 2) + "px";
 }
 
+function promptDistance() {
+	while(true) {
+		const input = prompt("Enter distance");
+		if(input === null)
+			return null;
+		const distance = +input;
+		if(!isNaN(distance))
+			return distance;
+	}
+}
 
+function createLine(ip1, ip2) {
+	const distance = promptDistance();
+	if(distance === null)
+		return null;
+	network.setConnection(ip1, ip2, distance);
+	const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	connections.set(getConnectionString(ip1, ip2), line);
+	connections.set(getConnectionString(ip2, ip1), line);
+	SVG.appendChild(line);
+	return line;
+}
 
+function moveLineBetween(line, element1, element2) {
+	line.setAttribute("x1", AREA.offsetLeft + element1.offsetLeft + element1.offsetWidth / 2);
+	line.setAttribute("y1", AREA.offsetTop  + element1.offsetTop + element1.offsetHeight / 2);
+	line.setAttribute("x2", AREA.offsetLeft + element2.offsetLeft + element2.offsetWidth / 2);
+	line.setAttribute("y2", AREA.offsetTop  + element2.offsetTop + element2.offsetHeight / 2);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function inputAlert() {
-// 	if(NAME_TEXT.value.length == 0)
-// 		return "Name can't be empty";
-// 	if(IP_TEXT.value.length === 0)
-// 		return "IP can't be empty";
-// 	const name = net.getEndpointNameByIP(IP_TEXT.value);
-// 	if(name !== undefined)
-// 		return `IP already assigned to ${name}`;
-// 	return null;
-// }
-// 
-// function clearedInputIfValid() {
-// 	const msg = inputAlert();
-// 	if(msg !== null) {
-// 		alert(msg);
-// 		return null;
-// 	}
-// 	const name = NAME_TEXT.value;
-// 	const ip = IP_TEXT.value;
-// 	NAME_TEXT.value = "";
-// 	IP_TEXT.value = "";
-// 	return [name, ip];
-// }
-// 
-// const net = new Network;
-// const nodes = new Map; // ip -> element
-// 
-// function createNode(name, ip, x, y) {
-// 	const nameSpan = document.createElement("span");
-// 	nameSpan.textContent = name;
-// 	const ipSpan = document.createElement("span");
-// 	ipSpan.textContent = ip;
-// 	const div = document.createElement("div");
-// 	nodes.set(ip, div);
-// 	div.style.position = "absolute";
-// 	div.style.left = x + "px";
-// 	div.style.top = y + "px";
-// 	div.appendChild(nameSpan);
-// 	div.appendChild(document.createElement("br"));
-// 	div.appendChild(ipSpan);
-// 	AREA_DIV.appendChild(div);
-// }
-// 
-// function areaDivClick(event) {
-// 	const input = clearedInputIfValid();
-// 	if(input === null)
-// 		return;
-// 	const [name, ip] = input;
-// 	if(ROUTER_CHECKBOX.checked)
-// 		net.addRouter(name, ip);
-// 	else
-// 		net.addEndpoint(name, ip);
-// 	createNode(name, ip, event.x, event.y);
-// }
+function getConnectionString(ip1, ip2) {
+	return `${ip1}:${ip2}`;
+}
